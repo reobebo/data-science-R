40d76288454da6d42ac0b8b3dbd8652715bd24f7
Apple <Apple@Apples-MBP-47.guest>
2017-12-18 22:08:14 -0600

    Crosstabs
    Creating crosstabs using a dataset based on social media sites used by different people based on gender.

---
+++ crosstabs.R
@@ -0,0 +1,21 @@
+# Load data file about social networks
+# Deleted one case with missing data on Gender
+sn <- read.csv("/Users/Apple/Documents/Lynda/Ex_Files_UaR_R/Exercise Files/Ch06/06_03/social_network.csv", header = T)
+names(sn)
+
+# Create contingency table
+sn.tab <- table(sn$Gender, sn$Site)
+sn.tab
+
+# If desired, can get marginal frequencies
+margin.table(sn.tab, 1) # Row marginal frequencies
+margin.table(sn.tab, 2) # Column marginal frequencies
+
+# Call also get cell, row, and column %
+# With rounding to get just 2 decimal places
+round(prop.table(sn.tab), 2)    # cell %
+round(prop.table(sn.tab, 1), 2) # row %
+round(prop.table(sn.tab, 2), 2) # column %
+
+# Chi-squared test
+chisq.test(sn.tab)
